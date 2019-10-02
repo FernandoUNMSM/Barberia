a66719dcfb7e8bb9863dbaccd53ff6ec338551bb
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2019-10-02 18:10:04 -0500

    mas cosas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 const menu = document.getElementById('menu-container');
-const slider = document.getElementById('slider')
+const slider = document.getElementById('slider');
+var a = document.getElementsByClassName('item-a');
+const span = document.querySelectorAll('.menu-item span');
+setInterval(() => slider.classList.toggle('nuevo'), 2000);
 
-setInterval(()=>slider.classList.toggle('nuevo'),2000);
+window.onscroll = () => {
+    if (window.pageYOffset > 0) {
+        menu.classList.add('fuera');
+        for (let i=0; i<a.length;i++) {
+            a[i].style.color="black";
+            span[i].style.backgroundColor="black";
+        }
+    }
+    else {
+        menu.classList.remove('fuera');
+        for (let i=0; i<a.length;i++) {
+            a[i].style.color="white";
+            span[i].style.backgroundColor="white";
+        }
+    }
+}
 
 

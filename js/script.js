094c94b84b2787c0a4126aac4a8ade11a42adc99
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2019-10-02 15:56:38 -0500

    mas cosas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-const altura = document.body.scrollHeight - window.innerHeight;
 const menu = document.getElementById('menu-container');
 const slider = document.getElementById('slider')
-
-if (window.pageYOffset > 10) {
-    //menu.style.backgroundColor = '#fff';
-}
 
 setInterval(()=>slider.classList.toggle('nuevo'),2000);
 
